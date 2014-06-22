f7df466f1a1a8aee31498106d42784eb634b43d1
xjsXjtu <xjs.xjtu@gmail.com>
2014-06-22 12:27:14 +0800

    [reverseInt] O(n) , AC~

---
+++ reverseInt/reverseInt/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
+class Solution {
+public:
+    int reverse(int x)
+    {
+        long long out = 0;
+        while(x)
+        {
+            out = out * 10 + x % 10;
+            assert(out < INT_MAX);
+            x = x / 10;
+        }
+        if(x<0)
+            return (int)-out;
+        else
+            return (int)out;
+    }
+};
+
+int main()
+{
+    int x1 = 123;
+    int x2 = -123;
+    int x3 = 1000000003; // failed
+
+    cout << INT_MAX << endl; 
+
+    Solution s;
+    cout << s.reverse(x3) << endl;
+
+    system("pause");
+    return 0;
+}
