18721182a46a781a9f9d6f1d184ef83383a75c18
xjsXjtu <xjs.xjtu@gmail.com>
2015-05-13 21:28:34 +0800

    [solveSudoku] AC~

--- 0_leetcode_others/0_leetcode_others/solveSudoku.cpp
+++ 0_leetcode_others/0_leetcode_others/solveSudoku.cpp
@@ -5,46 +5,58 @@
 #include <assert.h>
 using namespace std;
 
+
+void printSudoku(vector<vector<char>>& board);
+
 class Solution {
 private:
-	set<int> set_square[9];
-	set<int> set_horizon[9];
-	set<int> set_vertical[9];
-
-private:
-	bool isValidNum(int x, int y, int val)
+	bool isValidNum(int x, int y, int val, set<int> set_square[9], set<int> set_horizon[9], set<int> set_vertical[9])
 	{
 		if(
 			set_square  [x / 3 * 3 + y / 3].find(val) == set_square  [x / 3 * 3 + y / 3].end()
 		 && set_horizon [x                ].find(val) == set_horizon [x                ].end()
 		 && set_vertical[y                ].find(val) == set_vertical[y                ].end()
 		 )
-		 return true;
+			return true;
 		return false;
 	}
 
-	void solve(vector<vector<char>>& board, int blank_num, int lastx, int lasty)
+	bool solve(vector<vector<char>>& board, int blank_num, int lastx, int lasty, set<int> set_square[9], set<int> set_horizon[9], set<int> set_vertical[9])
 	{
+		//cout << endl << "~~~~~~~~~~~~~~~" << "x, y, blanks = " << lastx <<", " << lasty << ", " << blank_num << endl;
+		//printSudoku(board);
+
+		if(blank_num == 0)
+			return true;
+
 		int x, y;
 		for(x=lastx; x<9; x++)		// horizon
 		{
-			for(y=lasty; y<9; y++)	// vertical
+			for(y=0; y<9; y++)	// vertical
 			{
 				if(board[x][y] == '.')
-					break;
+					goto EXIT_LOOPS;
 			}
 		}
-		if(x==9 && y==9 && board[8][8] != '.')
-			return;
+EXIT_LOOPS:
 
-		for(int val=0; val<10; val++)
+		for(int val=1; val<10; val++)
 		{
-			if(isValidNum(x, y, val))
+			if(isValidNum(x, y, val, set_square, set_horizon, set_vertical))
 			{
 				board[x][y] = val + '0';
-				return solve(board, blank_num - 1, x, y);
+				set_square  [x / 3 * 3 + y / 3].insert(val);
+				set_horizon [x                ].insert(val);
+				set_vertical[y                ].insert(val);
+				if(solve(board, blank_num - 1, x, y, set_square, set_horizon, set_vertical))
+					return true;;
+				board[x][y] = '.';
+				set_square  [x / 3 * 3 + y / 3].erase(val);
+				set_horizon [x                ].erase(val);
+				set_vertical[y                ].erase(val);
 			}
 		}
+		return false;
 	}
 
 public:
@@ -55,17 +67,28 @@ public:
 		assert(board[0].size() == 9);
 		int blanks = 0;
 		int firstx = -1, firsty = -1;
+		
+		set<int> set_square[9];
+		set<int> set_horizon[9];
+		set<int> set_vertical[9];
 		for(int i=0; i<9; i++)
 		{
 			set_square[i].clear();
 			set_horizon[i].clear();
 			set_vertical[i].clear();
 		}
+
 		for(int x=0; x<9; x++)		// horizon
 		{
 			for(int y=0; y<9; y++)	// vertical
 			{
 				if(board[x][y] != '.')
+				{
+					set_square  [x / 3 * 3 + y / 3].insert(board[x][y] - '0');
+					set_horizon [x                ].insert(board[x][y] - '0');
+					set_vertical[y                ].insert(board[x][y] - '0');
+				}
+				else
 				{
 					if(firstx == -1)
 					{
@@ -73,15 +96,12 @@ public:
 						firsty = y;
 					}
 					blanks++;
-					set_square  [x / 3 * 3 + y / 3].insert(board[x][y] - '0');
-					set_horizon [x                ].insert(board[x][y] - '0');
-					set_vertical[y                ].insert(board[x][y] - '0');
 				}
 			}
 		}
 
 		// solve
-		solve(board, blanks, firstx, firsty);
+		solve(board, blanks, firstx, firsty, set_square, set_horizon, set_vertical);
 		return;
     }
 };
@@ -119,6 +139,11 @@ int main()
 	board[6][1] = '6'; board[6][6] = '2'; board[6][7] = '8';
 	board[7][3] = '4'; board[7][4] = '1'; board[7][5] = '9'; board[7][8] = '5';
 	board[8][4] = '8'; board[8][7] = '7'; board[8][8] = '9';
+	printSudoku(board);
+
+	Solution s;
+	s.solveSudoku(board);
+	cout << endl << "after solve" << endl;
 	printSudoku(board);
 
 	system("pause");
