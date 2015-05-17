a805299a4f2b160ce5aea567333dbf6e011441c1
xjsXjtu <xjs.xjtu@gmail.com>
2015-05-17 22:50:11 +0800

    [grayCode] math~~

--- 0_leetcode_others/0_leetcode_others/GrayCode.cpp
+++ 0_leetcode_others/0_leetcode_others/GrayCode.cpp
@@ -4,40 +4,13 @@
 using namespace std;
 
 class Solution {
-private:
-    void recursive(int n, vector<int> &out)
-    {
-        
-
-    }
 public:
-    vector<int> grayCode(int n) 
-    {
-        vector<int> out, out2;
-        if(n == 0)
-        {
-            return vector<int>(1, 0);
-        }
-        if(n == 1)
-        {
-            out.resize(2);
-            out[0] = 0;
-            out[1] = 1;
-            return out;
-        }
-        out2 = grayCode(n - 1);
-        int len2 = out2.size();
-        out.resize(len2 * 2);
-        for(int i=0; i < len2; i++)
-        {
-            out[i] = out2[i];
-        }
-        int add = 1 << (n - 1);
-        for(int i=0; i<len2; i++)
-        {
-            out[len2 + i] = out[len2 - 1 - i] + add;
-        }
-        return out;
+    vector<int> grayCode(int n) {
+        vector<int> result(1 << n, 0);
+        int len = 1 << n;
+        for (int i = 0; i < len; ++i)
+            result[i] = (i >> 1) ^ i;
+        return result;
     }
 };
 
