5f7021b89bd6b65a9465045fa0d1436632f3abda
xjsXjtu <xjs.xjtu@gmail.com>
2015-05-17 18:28:15 +0800

    [PermutationSequence] AC~ Math property.

--- 0_leetcode_others/0_leetcode_others/PermutationSequence.cpp
+++ 0_leetcode_others/0_leetcode_others/PermutationSequence.cpp
@@ -1,46 +1,49 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include "shared_functions.h"
+#include <algorithm>
 using namespace std;
 
 class Solution {
 private:
-    bool permuteCore(vector<int>& nums, int start, string& path, int &seq, int k)
+    int factorial_tab[10];
+
+public:
+    Solution()
     {
-        if(start == nums.size())
+        factorial_tab[0] =   1      ;   // 0!
+        factorial_tab[1] =   1      ;   // 1!
+        factorial_tab[2] =   2      ;   // 2!
+        factorial_tab[3] =   6      ;   // 3!
+        factorial_tab[4] =   24     ;
+        factorial_tab[5] =   120    ;
+        factorial_tab[6] =   720    ;
+        factorial_tab[7] =   5040   ;
+        factorial_tab[8] =   40320  ;
+        factorial_tab[9] =   362880 ;
+    }
+    void core(int n, int k, vector<char> nums, string& path)
+    {
+        cout << "k, " << k << endl;
+        if(n == 1)
         {
-            // cout << path << endl;
-            seq++;
-            if(seq == k)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            path.push_back(nums[nums.size() - 1]);
+            return;
         }
-        for(int i=start; i<nums.size(); i++)
-        {
-            path.push_back(nums[i]);
-            swap(nums[start], nums[i]);
-            if(permuteCore(nums, start + 1, path, seq, k))
-                return true;
-            path.pop_back();
-            swap(nums[start], nums[i]);
-        }
-        return false;
+        int x = (k - 1) / factorial_tab[n-1];
+        path.push_back(nums[nums.size() - n + x]);
+        swap(nums[nums.size() - n + x], nums[nums.size() - n]);
+        sort(nums.begin() + nums.size() - n + 1, nums.end());
+        core(n-1, k - x * factorial_tab[n-1], nums, path);
     }
 public:
     string getPermutation(int n, int k)
     {
         string path;
-        vector<int> nums;
-        int seq = 0;
+        vector<char> nums;
         for(int i=1; i<n+1; i++)
             nums.push_back(i + '0');
-        permuteCore(nums, 0, path, seq, k);
+        core(n, k, nums, path);
         return path;
     }
 };
@@ -48,7 +51,7 @@ public:
 int main()
 {
     Solution s;
-    cout << endl << s.getPermutation(3, 3) << endl;
+    cout << endl << s.getPermutation(3, 5) << endl;
 
     system("pause");
     return 0;
